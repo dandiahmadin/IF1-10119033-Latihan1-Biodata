de28baede4eb40143bce1f84391e131906ad1413
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2020-10-16 09:46:30 +0700

    Initial Commit

---
+++ src/if1/pkg10119033/latihan1/biodata/IF110119033Latihan1Biodata.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119033.latihan1.biodata;
+
+/**
+ *
+ * @author Dandi Ahmadin
+ */
+public class IF110119033Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-----------------------------------------------------");
+        System.out.println("NAMA\t\t: Dandi Ahmadin");
+        System.out.println("ALAMAT\t\t: Kp. Jatimekar No. 12 Rw. 03 Rw. 06");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 14 Juni 2000");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("HOBI\t\t: Sepeda & Lari");
+        System.out.println("MAKES\t\t: Soto Ayam");
+        System.out.println("MIKES\t\t: Jus Alpukat");
+    }
+    
+}
